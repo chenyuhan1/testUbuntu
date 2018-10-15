c591417209772d0d09076bf5e0c68c31e9c72e3f
chenyuhan1 <1944614333@qq.com>
2018-10-15 22:21:20 +0800

    test

--- coding-learning/first/test.js
+++ coding-learning/first/test.js
@@ -11,3 +11,13 @@ var sum = _.reduce([1, 2, 3], function (memo, num) {
     return memo+num;
 }, 0);
 console.log(sum);
+
+var founders = new Map();
+founders.set('facebook', 'mark');
+founders.set('google', 'larry');
+founders.size;
+founders.get('twitter');
+founders.has('yahoo');
+for (let [key, value] of founders) {
+    console.log(key + ' founded by ' + value);
+}
